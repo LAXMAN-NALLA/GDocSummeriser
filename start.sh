b9dfa287e11183e45ec0538dcde218326e9898d8
LAXMAN <146653479+LAXMAN-NALLA@users.noreply.github.com>
2025-08-05 16:22:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 PORT=${PORT:-8000}
-
-# Get the number of available CPU cores. Default to 1 if nproc is not available.
 CORES=$(nproc 2>/dev/null || echo 1)
-
-# Calculate the number of Gunicorn workers based on cores.
 WORKER_COUNT=$((2 * CORES + 1))
+if [[ $WORKER_COUNT -gt 4 ]]; then
+    WORKER_COUNT=3
+fi
 
 echo "Starting Gunicorn with $WORKER_COUNT workers on port $PORT"
 
-# Start Gunicorn with dynamic port
 exec gunicorn main:app \
     --workers $WORKER_COUNT \
     --worker-class uvicorn.workers.UvicornWorker \
